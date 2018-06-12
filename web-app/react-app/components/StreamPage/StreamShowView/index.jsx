2be94b5f360162644dfac39d3f2b3124661d4908
Aapeli <aapeli.haanpuu@streamr.com>
2018-06-12 14:17:06 +0200

    Add some data fetching on mount

--- web-app/react-app/components/StreamPage/StreamShowView/index.jsx
+++ web-app/react-app/components/StreamPage/StreamShowView/index.jsx
@@ -2,9 +2,12 @@
 
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {Row, Col} from 'react-bootstrap'
 import {StreamrBreadcrumb, StreamrBreadcrumbItem} from '../../Breadcrumb'
 import createLink from '../../../helpers/createLink'
-import {Row, Col} from 'react-bootstrap'
+import {getCurrentUser} from '../../../actions/user'
+import {getMyStreamPermissions, getStream, openStream} from '../../../actions/stream'
+
 import InfoView from './InfoView'
 import KeyView from './KeyView'
 import FieldView from './FieldView'
@@ -18,11 +21,46 @@ type StateProps = {
     stream: ?Stream
 }
 
-type Props = StateProps
+type DispatchProps = {
+    getStream: (id: $ElementType<Stream, 'id'>) => void,
+    openStream: (id: $ElementType<Stream, 'id'>) => void,
+    getMyStreamPermissions: (id: $ElementType<Stream, 'id'>) => void,
+    getCurrentUser: () => void
+}
+
+type RouterProps = {
+    match: {
+        params: {
+            id: string
+        }
+    }
+}
+
+type Props = StateProps & DispatchProps & RouterProps
 
 import styles from './streamShowView.pcss'
 
 export class StreamShowView extends Component<Props> {
+
+    componentDidMount() {
+        const id = this.props.match.params.id
+        this.props.getCurrentUser()
+        this.updateStream(id)
+    }
+
+    componentWillReceiveProps(newProps: Props) {
+        const id = newProps.match.params.id
+        if (!this.props.stream || id !== this.props.stream.id) {
+            this.props.getCurrentUser()
+            this.updateStream(id)
+        }
+    }
+
+    updateStream = (id: $ElementType<Stream, 'id'>) => {
+        this.props.getStream(id)
+        this.props.openStream(id)
+        this.props.getMyStreamPermissions(id)
+    }
 
     render() {
         return (
@@ -65,4 +103,19 @@ const mapStateToProps = ({stream}: {stream: StreamState}): StateProps => ({
     stream: stream.openStream.id ? stream.byId[stream.openStream.id] : null
 })
 
-export default connect(mapStateToProps)(StreamShowView)
+const mapDispatchToProps = (dispatch: Function): DispatchProps => ({
+    getStream(id: $ElementType<Stream, 'id'>) {
+        dispatch(getStream(id))
+    },
+    openStream(id: $ElementType<Stream, 'id'>) {
+        dispatch(openStream(id))
+    },
+    getMyStreamPermissions(id: $ElementType<Stream, 'id'>) {
+        dispatch(getMyStreamPermissions(id))
+    },
+    getCurrentUser() {
+        dispatch(getCurrentUser())
+    }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(StreamShowView)
--- web-app/react-app/components/StreamPage/index.jsx
+++ web-app/react-app/components/StreamPage/index.jsx
@@ -5,8 +5,6 @@ import {connect} from 'react-redux'
 import {Helmet} from 'react-helmet'
 import {withRouter} from 'react-router-dom'
 import type {Stream} from '../../flowtype/stream-types'
-import {getCurrentUser} from '../../actions/user'
-import {getMyStreamPermissions, getStream, openStream} from '../../actions/stream'
 
 import type {Node} from 'react'
 import type {StreamState} from '../../flowtype/states/stream-state'
@@ -19,37 +17,11 @@ type StateProps = {
     stream: ?Stream
 }
 
-type DispatchProps = {
-    getStream: (id: $ElementType<Stream, 'id'>) => void,
-    openStream: (id: $ElementType<Stream, 'id'>) => void,
-    getMyStreamPermissions: (id: $ElementType<Stream, 'id'>) => void,
-    getCurrentUser: () => void
-}
-
-type RouterProps = {
-    match: {
-        params: {
-            id: string
-        }
-    }
-}
-
-type Props = GivenProps & StateProps & DispatchProps & RouterProps
+type Props = GivenProps & StateProps
 
 type State = {}
 
 export class StreamPage extends Component<Props, State> {
-
-    componentWillReceiveProps(newProps: Props) {
-        const id = newProps.match.params.id
-        if (!this.props.stream || id !== this.props.stream.id) {
-            this.props.getStream(id)
-            this.props.openStream(id)
-            this.props.getMyStreamPermissions(id)
-            this.props.getCurrentUser()
-        }
-    }
-
     render() {
         return (
             <div style={{
@@ -69,19 +41,4 @@ const mapStateToProps = ({stream}: {stream: StreamState}): StateProps => ({
     stream: stream.openStream.id ? stream.byId[stream.openStream.id] : null
 })
 
-const mapDispatchToProps = (dispatch: Function): DispatchProps => ({
-    getStream(id: $ElementType<Stream, 'id'>) {
-        dispatch(getStream(id))
-    },
-    openStream(id: $ElementType<Stream, 'id'>) {
-        dispatch(openStream(id))
-    },
-    getMyStreamPermissions(id: $ElementType<Stream, 'id'>) {
-        dispatch(getMyStreamPermissions(id))
-    },
-    getCurrentUser() {
-        dispatch(getCurrentUser())
-    }
-})
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(StreamPage))
+export default withRouter(connect(mapStateToProps)(StreamPage))
